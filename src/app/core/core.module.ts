559508c348dfcbcfd193f998dd919aee8fdddd9c
NLuna19 <nluna190898@gmail.com>
2022-03-18 10:10:58 -0300

    added services

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { HttpService } from './services/http.service';
+import { PokemonService } from './services/pokemon.service';
 
+const service = [ AuthService, HttpService, PokemonService ]
 const modules = [];
 const components = [];
 
 @NgModule({
-  providers: [],
+  providers: [...service],
   imports: [
     CommonModule,
     RouterModule,
