0aec5ebdce526cedaab7c032746d8de867021db6
Nicolas Omar Luna <nluna190898@gmail.com>
2022-03-21 19:39:27 -0300

    Fixes (#6)
    * fix error imports

--- src/app/features/public/public.module.ts
+++ src/app/features/public/public.module.ts
@@ -14,8 +14,6 @@ import { TitleComponent } from '../public/components/title/title.component';
 import { InlinePokemonInfoComponent } from '@features/public/components/inline-pokemon-info/inline-pokemon-info.component';
 import { InlineSimpleOneComponent } from '@features/public/components/inline-simple-one/inline-simple-one.component'
 import { AbilitiesListComponent } from '@features/public/components/abilities-list/abilities-list.component';
-import { InputChipsComponent } from './components/input-chips/input-chips.component';
-
 
 const components = [ 
   PublicComponent, 
@@ -29,7 +27,7 @@ const components = [
   InlinePokemonInfoComponent,
   InlineSimpleOneComponent,
   AbilitiesListComponent,
-  InputChipsComponent,
+  
 ];
 const modules = [ 
   CommonModule, 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -26,7 +26,6 @@ const components = [
   ],
   declarations: [
     ...components,
-
   ]
 })
 export class SharedModule { }
