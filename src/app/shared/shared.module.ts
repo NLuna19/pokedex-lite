49ec12be2c4e80b12cc4de965ee51b8d4ed97658
NLuna19 <nluna190898@gmail.com>
2022-03-21 15:47:06 -0300

    added header-info in shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,16 @@ import { MaterialModule } from '@app/shared/materials/material.module';
 import { SharedTitleComponent } from './components/title/shared-title.component';
 import { InlineChipComponent } from './components/inline-chip/inline-chip.component';
 import { CustomImageComponent } from './components/custom-image/custom-image.component';
+import { HeaderInfoComponent } from './components/header-info/header-info.component';
 
 const modules = [ MaterialModule ];
-const components = [ SharedTitleComponent, InlineChipComponent, CustomImageComponent,];
+const components = [ 
+  SharedTitleComponent, 
+  InlineChipComponent, 
+  CustomImageComponent, 
+  HeaderInfoComponent,
+   
+];
 
 @NgModule({
   imports: [
@@ -18,7 +25,8 @@ const components = [ SharedTitleComponent, InlineChipComponent, CustomImageCompo
     ...components,
   ],
   declarations: [
-    ...components
+    ...components,
+
   ]
 })
 export class SharedModule { }
