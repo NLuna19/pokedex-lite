12ca62ec69287c4d3b74d39adb4fa685442385e5
NLuna19 <nluna190898@gmail.com>
2022-03-21 00:01:19 -0300

    changed directories in public folder

--- src/app/features/public/components/inline-pokemon-info/inline-pokemon-info.component.ts
+++ src/app/features/public/components/inline-pokemon-info/inline-pokemon-info.component.ts
@@ -1,10 +1,11 @@
+import { Router } from '@angular/router';
 import { Pokemon } from '@core/models/pokemon.models';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-
+import { Component, EventEmitter, Input, OnInit, Output, ChangeDetectionStrategy} from '@angular/core';
 @Component({
   selector: 'inline-pokemon-info',
   templateUrl: './inline-pokemon-info.component.html',
-  styleUrls: ['./inline-pokemon-info.component.scss']
+  styleUrls: ['./inline-pokemon-info.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InlinePokemonInfoComponent implements OnInit {
   @Input() data?: Pokemon[];
