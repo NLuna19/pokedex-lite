f312c19b4de611d036991979557c324d7c1ef977
NLuna19 <nluna190898@gmail.com>
2022-03-21 00:24:10 -0300

    chaged styles in shared-title.components and inline.components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@app/shared/materials/material.module';
 import { SharedTitleComponent } from './components/title/shared-title.component';
+import { InlineChipComponent } from './components/inline-chip/inline-chip.component';
 
 const modules = [ MaterialModule ];
-const components = [ SharedTitleComponent ];
+const components = [ SharedTitleComponent, InlineChipComponent ];
 
 @NgModule({
   imports: [
@@ -16,7 +17,7 @@ const components = [ SharedTitleComponent ];
     ...components,
   ],
   declarations: [
-    ...components
+    ...components,
   ]
 })
 export class SharedModule { }
